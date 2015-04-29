1096ddc6d67c249800b6535ecbee6081c6de63c6
Mariya <mbeketova@rambler.ru>
2015-04-29 11:59:06 +0300

    Initial Commit

---
+++ ObjectiveC2_Lesson2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjectiveC2_Lesson2
+//
+//  Created by Admin on 29.04.15.
+//  Copyright (c) 2015 Mariya Beketova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ObjectiveC2_Lesson2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjectiveC2_Lesson2
+//
+//  Created by Admin on 29.04.15.
+//  Copyright (c) 2015 Mariya Beketova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
